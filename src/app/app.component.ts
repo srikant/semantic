8f158d1e3d34ac77cbdcc8ff9ed4d378dc0666a4
Dhondi Srikanth <dhondi.srikanth@gmail.com>
2019-02-11 01:30:53 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'semantic';
+  constructor(private elemRef: ElementRef, private renderer: Renderer2) {
+
+  }
+  ngOnInit() {
+   /*  const dropdown: any = this.elemRef.nativeElement.getElementByClassName('dropdown'); //.querySelectorAll('.dropdown');
+    dropdown.dropdown({
+      maxSelections: 3
+    }); */
+
+  }
+
+  ngAfterViewInit() {
+    $('.ui.normal.dropdown')
+    .dropdown({
+      maxSelections: 3
+    });
+  $('.ui.special.dropdown')
+    .dropdown({
+      useLabels: false,
+      maxSelections: 3
+    });
+  }
 }
